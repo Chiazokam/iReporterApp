83a8d30864dc51c2a2ca5aadd525bf4d92f41562
chiazokam <chiazokamecheta@gmail.com>
2018-11-30 16:13:11 +0100

    Create the api endpoint for the about page

--- API/test/testApp.js
+++ API/test/testApp.js
@@ -3,11 +3,21 @@ const app = require('../app')
 const request = require("request");
 
 const base_url = "http://localhost:3000/api/v1";
+const about_url = "http://localhost:3000/api/v1/about";
 
 describe('iReporter', function(){
   describe("GET /", function() {
     it("Root page returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
+        assert.equal(200, response.statusCode);
+        done();
+      });
+    });
+  });
+
+  describe("GET /about", function() {
+    it("About page returns status code 200", function(done) {
+      request.get(about_url, function(error, response, body) {
         assert.equal(200, response.statusCode);
         done();
       });
