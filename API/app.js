9f34f466105bcbf1fdb611ccb05c705981a5511f
chiazokam <chiazokamecheta@gmail.com>
2018-11-30 15:52:16 +0100

    Change the root route to include the api version

--- API/app.js
+++ API/app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res){
+app.get('/api/v1', function(req, res){
   res.render('home.ejs');
 })
 
