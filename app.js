1942139094e4fd315a512a0a7a71d6f670d343cf
chiazokam <chiazokamecheta@gmail.com>
2018-12-01 03:01:00 +0100

    Amend the api endpoints for the home and about GET routes

--- app.js
+++ app.js
@@ -1,58 +1,22 @@
-const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
+app.use(express.static('public'));
 
-let users = [];
+/* HomePage Endpoint */
+app.get('/api/v1', (req, res) => res.status(200).send({ data: [] }));
 
-/*HomePage Endpoint */
-app.get('/api/v1', function(req, res){
-  res.render('home.ejs');
-})
+/* About Page Endpoint */
+app.get('/api/v1/about', (req, res) => res.status(200).send({ data: [] }));
 
-/*About Page Endpoint */
-app.get('/api/v1/about', function(req, res){
-  res.render('about.ejs');
-})
-
-/*SignUp Page Endpoint*/
-app.get('/api/v1/users/new', function(req, res){
-  res.render('signup.ejs');
-});
-
-/*SignUp Post */
-app.post('/api/v1/users', function(req, res){
-  let user = {
-    userId: users.length + 1,
-    firstname: req.body.fname,
-    lastname: req.body.lname,
-    othername: req.body.othername,
-    email: req.body.email,
-    phone: req.body.phone,
-    username: req.body.username,
-    registered: true,
-    isAdmin: req.body.admin
-  }
-if(!user.firstname || !user.lastname || !user.othername || !user.email || !user.phone || !user.username){
-  res.json({
-    status: "400",
-    error: "No user data captured"
-  })
-} else {
-  res.json({
-    status: "201",
-    data: [user]
-  });
-  users.push(user);
-};
-})
 
 module.exports = app;
-/****************************************************************/
+/* ************************************************************** */
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port);
--- test/testApp.js
+++ test/testApp.js
@@ -1,58 +1,34 @@
 const chai = require('chai');
-const app = require('../app')
 const request = require('supertest');
+const app = require('../app');
+
 const expect = chai.expect;
 
-const base_url = "http://localhost:3000/api/v1";
-const about_url = "http://localhost:3000/api/v1/about";
-const get_signup_url = "http://localhost:3000/api/v1/users/new";
 
-describe('iReporter', function(){
-  let user = {
-    userId: 1,
-    firstname: 'Amarachi',
-    lastname: 'David',
-    othername: 'Stella',
-    email: 'stelladavid@amarachi.com',
-    phone: '09049445633',
-    username: 'Mara',
-    registered: true,
-    isAdmin: 'false'
-  }
-  /*describe("GET /", function() {
-    it("Root page returns status code 200", function(done) {
-      request.get(base_url, function(error, response, body) {
-        assert.equal(200, response.statusCode);
-        done();
-      });
-    });
-  });
-
-  describe("GET /about", function() {
-    it("About page returns status code 200", function(done) {
-      request.get(about_url, function(error, response, body) {
-        assert.equal(200, response.statusCode);
-        done();
-      });
-    });
-  });*/
-
-  /*describe("GET /users/new", function() {
-    it("Sign Up page returns status code 200", function(done) {
-      request.get(get_signup_url, function(error, response, body) {
-        assert.equal(200, response.statusCode);
-        done();
-      });
-    });*/
-  describe("POST api/v1/users", function() {
-    it('should add the new user', function(done){
+describe ('GET Requests', () => {
+  describe ('GET /api/v1', () => {
+    it('should get the homepage', (done) => {
       request(app)
-      .post('/api/v1/users')
-      .send(user)
-      .end(function(err, res){
-        expect(res.statusCode).to.equal(200);
-        done(err)
-      });
+        .get('/api/v1')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.be.empty;
+          done();
+        });
     });
-  });
-});
+  })
+
+  describe ('GET /api/v1/about', () => {
+    it('should get the about page', (done) => {
+      request(app)
+        .get('/api/v1/about')
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.data).to.be.empty;
+          done();
+        });
+    });
+  })
+})
