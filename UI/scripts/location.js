78d8c6bcb4fa0d7fa187589d18882e8a499374e2
chiazokam <chiazokamecheta@gmail.com>
2019-01-11 19:53:29 +0100

    merge files

--- UI/scripts/location.js
+++ UI/scripts/location.js
@@ -1,12 +1,32 @@
-function initMap(){
-    let myLatLng = {lat: 6.500000, lng: 3.350000};
-    let map = new google.maps.Map(document.querySelector('.map'), {
-            center: myLatLng, 
-            zoom: 4
-          });
-    let marker = new google.maps.Marker({
-            map: map,
-            position: myLatLng
-          });
-  }
-  
+function initMap(latitude, longitude){
+        let myLatLng = {lat: latitude, lng: longitude};
+        let map = new google.maps.Map(document.querySelector('.map'), {
+                center: myLatLng, 
+                zoom: 4
+              });
+        let marker = new google.maps.Marker({
+                map: map,
+                position: myLatLng
+              });
+      }
+
+function loadMap() {
+        fetch('https://ireporter-heroku.herokuapp.com/api/v1/redflags', {
+        headers: {
+                'token': token,
+        }
+        })
+        .then((res) => res.json())
+        .then((response) => {
+        const { data } = response;
+        data.forEach((record) => {
+                let newLocation = record.location.split(',');
+                let lat = Number(newLocation[0]);
+                let long = Number(newLocation[1]);
+
+                initMap(lat, long);
+                })
+        })
+}
+
+window.onload = loadMap;
--- UI/scripts/viewRedflags.js
+++ UI/scripts/viewRedflags.js
@@ -58,17 +58,4 @@ fetch('https://ireporter-heroku.herokuapp.com/api/v1/redflags', {
     }
 });
 
-
-
-function initMap(latitude, longitude){
-    let myLatLng = {lat: latitude, lng: longitude};
-    let map = new google.maps.Map(document.querySelector('.map'), {
-            center: myLatLng, 
-            zoom: 4
-          });
-    let marker = new google.maps.Marker({
-            map: map,
-            position: myLatLng
-          });
-  }
   
